4a47afd82e56366221f9349f2b1256384708eac5
yerti <yerti0818@gmail.com>
2023-07-11 12:08:10 -0700

    task13

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 alphabet = 122
 while alphabet > 96:
-    print("{}".format(chr(alphabet)), end='')
+    if alphabet % 2 == 0:
+        print("{}".format(chr(alphabet)), end='')
+    elif alphabet % 2 != 0:
+        print("{}".format(chr(alphabet - 32)), end="")
     alphabet -= 1
