8ca20bc4191f49e656bf68dc3607dfa8e0df619a
yerti <yerti0818@gmail.com>
2023-07-13 15:43:22 -0700

    task02

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,8 +7,10 @@ args = sys.argv[1:]
 num_args = len(args)
 
 if num_args == 0:
+    print("{} arguments.".format(num_args))
+elif num_args == 1:
     print("{} argument:".format(num_args))
 else:
-    print("{} argument:".format(num_args))
-    for i, arg in enumerate(args, 1):
-        print("{}: {}".format(i, arg))
+    print("{} arguments:".format(num_args))
+for i, arg in enumerate(args, 1):
+    print("{}: {}".format(i, arg))
