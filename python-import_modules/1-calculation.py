0e129ae7f747fdf28cde52fa3306d64d920f5248
yerti <yerti0818@gmail.com>
2023-07-13 14:59:43 -0700

    task01

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+resultadd = add(a,b)
+print("{} + {} = {}".format(a, b, resultadd))
+
+resultsub = sub(a,b)
+print("{} - {} = {}".format(a, b, resultsub))
+
+resultmul = mul(a,b)
+print("{} * {} = {}".format(a, b, resultmul))
+
+resultdiv = div(a,b)
+print("{} / {} = {}".format(a, b, resultdiv))
