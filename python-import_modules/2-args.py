68167462736158cf93de4a2267734c3878e32832
yerti <yerti0818@gmail.com>
2023-07-13 15:30:48 -0700

    tas02

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+
+args = sys.argv[1:]
+num_args = len(args)
+
+if num_args == 0:
+    print("{} argument:".format(num_args))
+else:
+    print("{} argument:".format(num_args))
+    for i, arg in enumerate(args, 1):
+        print("{}: {}".format(i, arg))
