f9ff856cef3451dba20982641a5357433ede48c1
yerti <yerti0818@gmail.com>
2023-07-13 15:03:36 -0700

    task01

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,14 +5,14 @@ if __name__ == "__main__":
 
 a = 10
 b = 5
-resultadd = add(a,b)
+resultadd = add(a, b)
 print("{} + {} = {}".format(a, b, resultadd))
 
-resultsub = sub(a,b)
+resultsub = sub(a, b)
 print("{} - {} = {}".format(a, b, resultsub))
 
-resultmul = mul(a,b)
+resultmul = mul(a, b)
 print("{} * {} = {}".format(a, b, resultmul))
 
-resultdiv = div(a,b)
+resultdiv = div(a, b)
 print("{} / {} = {}".format(a, b, resultdiv))
