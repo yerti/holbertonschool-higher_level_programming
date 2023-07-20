7c0b3f63690f73d0c6cf63ea7253756e6dd71807
yerti <yerti0818@gmail.com>
2023-07-20 08:23:01 -0700

    task15

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
       for value in a_dictionary.values():
         for i, n in a_dictionary.items():
-            if n == "C":
+            if n == value:
                 del a_dictionary[i]
                 break
         return a_dictionary
