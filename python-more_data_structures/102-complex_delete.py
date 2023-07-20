8941989979b4acf338fa5c1dac09a602285868ef
yerti <yerti0818@gmail.com>
2023-07-20 08:06:28 -0700

    task15

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+      for value in a_dictionary.values():
+        for i, n in a_dictionary.items():
+            if n == "C":
+                del a_dictionary[i]
+                break
+        return a_dictionary
