2f69719cabba9d84f3462cbc97a4a64acbfd5574
yerti <yerti0818@gmail.com>
2023-07-20 06:13:14 -0700

    task12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,20 @@
 def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or roman_string is None):
         return (0)
-    numrs_romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    romans = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000}
     result = 0
     for n in range(len(roman_string)):
-        if numrs_romans.get(roman_string[n], 0) == 0:
+        if romans.get(roman_string[n], 0) == 0:
             return (0)
-        elif (n != (len(roman_string) - 1) and numrs_romans[roman_string[n]] < numrs_romans[roman_string[n + 1]]):
-            result += numrs_romans[roman_string[n]] * - 1
+        elif (n != (len(roman_string) - 1) and romans[roman_string[n]] < romans[roman_string[n + 1]]):
+            result += romans[roman_string[n]] * - 1
         else:
-            result += numrs_romans[roman_string[n]]
+            result += romans[roman_string[n]]
     return result
