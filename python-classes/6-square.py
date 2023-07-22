1d97a029344ef882af6db3480b66b04886b34e11
yerti <yerti0818@gmail.com>
2023-07-22 09:09:26 -0700

    task06

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -37,6 +37,10 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
+    def area(self):
+        result = self.__size * self.__size
+        return (result)
+
     def my_print(self):
         if self.__size == 0:
             print()
