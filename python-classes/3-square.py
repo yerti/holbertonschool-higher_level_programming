aeb287591b0ebb8cb2d970fad643a3b35418697f
yerti <yerti0818@gmail.com>
2023-07-22 06:43:58 -0700

    task03

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Create empty class square"""
+
+
+class Square:
+    """Defines class and instantiates private"""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """return of area of square"""
+        result = self.__size * self.__size
+        return (result)
