fcb654647d0ef6fb3f50884a4299f47cd7615648
yerti <yerti0818@gmail.com>
2023-07-24 12:55:34 -0700

    task01

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines empty class Rectangle"""
 
+
 class Rectangle:
     """Defines class and instanties private"""
-
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
@@ -22,7 +22,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -30,6 +30,6 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
