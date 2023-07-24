c93714e9e447d05d86927bac0ec7e2c9b33770c6
yerti <yerti0818@gmail.com>
2023-07-24 11:34:02 -0700

    task01

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Defines empty class Rectangle"""
+
+class Rectangle:
+    """Defines class and instanties private"""
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    """Getter that accesses private values"""
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    """Setter to update attribute value"""
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
