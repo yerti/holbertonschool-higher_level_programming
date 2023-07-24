66044314ccf8b747c8073cbeaf4efbd68cfd88b2
yerti <yerti0818@gmail.com>
2023-07-24 14:14:27 -0700

    task03

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -50,5 +50,6 @@ class Rectangle:
             for i in range(self.__height):
                 for j in range(self.__width):
                     rect += "#"
-                rect += "\n"
+                if i < self.__height - 1:
+                    rect += "\n"
             return rect
