63a6a396f25f9e634bf60824070f453ba11f9ef9
yerti <yerti0818@gmail.com>
2023-07-29 05:58:41 -0700

    adds files of test

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Integers addition"""
+
+
+def add_integer(a, b=98):
+    """adds two integers"""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    result = a + b
+    return (result)
