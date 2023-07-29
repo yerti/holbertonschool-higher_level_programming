f65505a7824b74b5802cb1482423f0775521aa39
yerti <yerti0818@gmail.com>
2023-07-29 09:11:28 -0700

    add file task04

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that return true or false"""
+
+def inherits_from(obj, a_class):
+    """ conditional of type and instanci that return true"""
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
