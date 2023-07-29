5d956773ee5f172dd97c869ec9a9770b0e644bbc
yerti <yerti0818@gmail.com>
2023-07-29 07:42:39 -0700

    add task file 02

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,15 +5,19 @@
 def matrix_divided(matrix, div):
 
     """Validate matrix"""
-    if not all(isinstance(row, list) for row in matrix) \
-        or not all(isinstance(elem, (int, float)) for row in matrix for elem in row):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+    is_matrix = isinstance(matrix, list)
+    error = "matrix must be a matrix (list of lists) of integers/floats"
+    if not all(isinstance(row, list) for row in matrix if is_matrix):
+        raise TypeError(error)
+
+    if not matrix:
+        raise TypeError("matrix must not be empty")
+
     size = len(matrix[0])
-    if not all(len(row) == size for  row in matrix):
+    if not all(len(row) == size for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    if not isinstance(div,(int, float)):
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
     if div == 0:
