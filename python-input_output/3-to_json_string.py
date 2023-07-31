aefde2dc539b58ce7abca851c81db1a2d8bdafd5
yerti <yerti0818@gmail.com>
2023-07-31 06:38:04 -0700

    Add file task 03

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,5 +1,10 @@
+#!/usr/bin/python3
+"""This module has function to json_string"""
+
+
 import json
 
 
 def to_json_string(my_obj):
+    """Function that return a json"""
     return json.dumps(my_obj)
