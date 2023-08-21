38998ee8730965060cc6c680f2fcbaf99e2ed69e
yerti <yerti0818@gmail.com>
2023-08-21 15:58:23 -0700

    Add file to create second table

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Creates a second table from the hbtn_0c_0 database
+CREATE TABLE second_table (
+	id INT PRIMARY KEY,
+	name VARCHAR(256),
+	score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
