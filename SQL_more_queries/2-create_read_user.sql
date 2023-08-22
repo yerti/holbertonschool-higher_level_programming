6e13f9ed67898f5e2ac6d632297bba68a3ce8560
yerti <yerti0818@gmail.com>
2023-08-22 15:08:03 -0700

    Feat: to create the user and give privileges

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- To create the data base hbtn_0d_2.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create the user user_0d_2.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+-- Give select only privilege to user user_0d_2.
+GRANT SELECT ON hbtn_od_2.* TO 'user_0d_2'@'localhost';
+-- The user_0d_2 password should be set to user_0d_2_pwd.
+ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
