1bb25b54cc8d5ff17d59677c82364e470ba074b5
yerti <yerti0818@gmail.com>
2023-08-22 14:29:06 -0700

    Feat script que crea un usuario y otorga privilegios

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Create the user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+-- Give user privileges user_0d_1 
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- The user_0d_1 password should be set to user_0d_1_pwd 
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1pwd';
