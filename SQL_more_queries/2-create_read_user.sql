89015131809f6ff03773230721f3a171c12eb1d8
yerti <yerti0818@gmail.com>
2023-08-23 05:45:57 -0700

    fix to remove project comments

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,5 @@
--- To create the data base hbtn_0d_2.
+-- create the user user 0_2 in the database hbtn_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- Create the user user_0d_2.
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
--- The user_0d_2 password should be set to user_0d_2_pwd.
 ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- Give select only privilege to user user_0d_2.
-GRANT SELECT ON hbtn_od_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
