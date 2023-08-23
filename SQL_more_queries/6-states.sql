a70ab2ae638e809abdc628f504f65cc11ab6ab1e
yerti <yerti0818@gmail.com>
2023-08-23 15:34:04 -0700

    Feat: script that creates a new database and a new table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Create a database hbtn_0d_usa.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+-- Create a table states.
+CREATE TABLE IF NOT EXISTS states (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+	);
+	
