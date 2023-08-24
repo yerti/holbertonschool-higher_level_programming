a0f26aa37c48ade3cc2008fa33537edc2bd82975
yerti <yerti0818@gmail.com>
2023-08-24 16:58:14 -0700

    Add file

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Lists all shows contained in the database hbtn_0d_tvshows
+SELECT tv_shows.title AS title, tv_show_genres.genre_id  AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title, genre_id ASC;
