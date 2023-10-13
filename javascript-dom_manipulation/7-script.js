91c265f9724c256fd11b6803b3ee3986c6fd70ad
yerti <yerti0818@gmail.com>
2023-10-13 10:21:02 -0500

    Feat: script that returns a list of movie titles

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,6 +1,6 @@
 const ulListMovies =  document.querySelector("#list_movies");
 
-fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+fetch('https://swapi-api.hbtn.io/api/films/?format=json') // URL Where do we get movie titles from
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -14,7 +14,7 @@ fetch('https://swapi-api.hbtn.io/api/films/?format=json')
             const titleMovie = movie.title;
             const list = document.createElement('li');
             list.textContent =  titleMovie;
-            ulListMovies.appendChild(list);
+            ulListMovies.appendChild(list); // It returns us the list of movie titles
         })
         .catch(error => {
             console.error('An error occurred:', error);
